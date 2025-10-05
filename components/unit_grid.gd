ab5feb008d2472dceada1540be0c99881050770d
miloscolic <milos.colic@4digital.io>
2025-10-05 18:23:30 +0200

    unit mover

--- components/unit_grid.gd
+++ components/unit_grid.gd
@@ -16,6 +16,15 @@ func add_unit(tile: Vector2i, unit: Node) -> void:
 	units[tile] = unit
 	unit_grid_changed.emit()
 
+func remove_unit(tile: Vector2i) -> void:
+	var unit := units[tile] as Node
+
+	if not unit:
+		return
+	
+	units[tile] = null
+	unit_grid_changed.emit()
+
 func is_tile_occupied(tile: Vector2i) -> bool:
 	return units[tile] != null
 
